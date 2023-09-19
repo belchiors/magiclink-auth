271bc24aa12f300538f8ac76c2b70b2a11106761
belchiors <belchiorsinacio@gmail.com>
2023-09-19 18:07:52 -0300

    Refactor controller so that href can be customized from appsettings.json

--- src/MagicLink.API/Controllers/IndexController.cs
+++ src/MagicLink.API/Controllers/IndexController.cs
@@ -15,18 +15,21 @@ public class IndexController : ControllerBase
     private readonly ITaskService _taskService;
     private readonly ITokenService _tokenService;
     private readonly ILogger<IndexController> _logger;
+    private readonly IConfiguration _configuration;
 
-    private readonly string _htmlTemplate = @"<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Login to Our Application</title></head><body><div style='max-width:600px;margin:0 auto;padding:20px;'><h1>Login to Our Application</h1><p>Click the button below to log in to our application using a magic link:</p><a href='{magicLink}' style='display:inline-block;padding:10px 20px;background-color:#007BFF;color:#ffffff;text-decoration:none;font-weight:bold;border-radius:5px;'>Login with Magic Link</a><p>If you did not request this login or have any questions, please contact our support team.</p></div></body></html>";
+    private readonly string _htmlTemplate = @"<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Login to Our Application</title></head><body><div style='max-width:600px;margin:0 auto;padding:20px;'><h1>Login to Our Application</h1><p>Click the button below to log in to our application using a magic link:</p><a href={href} style='display:inline-block;padding:10px 20px;background-color:#007BFF;color:#ffffff;text-decoration:none;font-weight:bold;border-radius:5px;'>Login with Magic Link</a><p>If you did not request this login or have any questions, please contact our support team.</p></div></body></html>";
 
     public IndexController(
         ITaskService taskService,
         ITokenService tokenService,
-        ILogger<IndexController> logger)
+        ILogger<IndexController> logger,
+        IConfiguration configuration)
     {
         _logger = logger;
 
         _taskService = taskService;
         _tokenService = tokenService;
+        _configuration = configuration;
     }
 
     [HttpPost("[action]")]
@@ -36,6 +39,17 @@ public class IndexController : ControllerBase
         {
             return BadRequest();
         }
+
+        string href = string.Empty;
+        try
+        {
+            href = _configuration["HostName"];
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(500);
+        }
         
         _logger.LogInformation("Generating web token");
         var token = _tokenService.GenerateToken(model.Email);
@@ -43,14 +57,15 @@ public class IndexController : ControllerBase
         _logger.LogInformation("Creating message");
         var message = new Message
         {
+            Subject = string.Empty,
             From = model.Email,
             To = model.Email,
-            Body = _htmlTemplate.Replace("magicLink", token)
+            Body = _htmlTemplate.Replace("{href}", $"href/{token}")
         };
 
         _taskService.SendMessage(Encoding.UTF8.GetBytes(message.ToJson()));
         
-        return Ok(token);
+        return Ok();
     }
 
     [HttpGet("[action]/{token}")]
